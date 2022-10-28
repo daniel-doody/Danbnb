b482ba49012de0ae454b28c72c232463f7decb5f
Daniel Doody <you@example.com>
2022-10-28 19:27:01 -0400

    infocard changes

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -6,7 +6,7 @@ function InfoCard({ img, location, title, description, star, price, total }) {
   return (
     <div
       className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg 
-    transition duration-200 ease-out first:border-t"
+    active:scale-90 transition duration-200 ease-out first:border-t"
     >
       {/* Left side - Image */}
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
